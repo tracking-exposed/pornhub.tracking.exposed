c571bb4736b13da64bca5fa45d8a7f9943c1cfda
Claudio <claudio@tracking.exposed>
2019-12-11 14:55:54 +0100

    fixed personal radar with squared lines and full category list

--- static/js/radarLoader.js
+++ static/js/radarLoader.js
@@ -1,35 +1,11 @@
-function render(data) { 
-  /* Radar chart design created by Nadieh Bremer - VisualCinnamon.com */
+function render(data, radarChartOptions) { 
 
-    ////////////////////////////////////////////////////////////// 
-    //////////////////////// Set-Up ////////////////////////////// 
-    ////////////////////////////////////////////////////////////// 
-
-    var margin = {top: 100, right: 100, bottom: 100, left: 100},
-        width = Math.min(700, window.innerWidth - 10) - margin.left - margin.right,
-        height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
-            
-    ////////////////////////////////////////////////////////////// 
-    //////////////////// Draw the Chart ////////////////////////// 
-    ////////////////////////////////////////////////////////////// 
-
-    var color = d3.scaleOrdinal()
-        .range(["#ffffff","#f98e05","#00A0B0"]);
-        
-    var radarChartOptions = {
-      w: width,
-      h: height,
-      margin: margin,
-      maxValue: 0.5,
-      levels: 5,
-      roundStrokes: true,
-      color: color
-    };
     //Call function to draw the Radar chart
     RadarChart(".radarChart", data, radarChartOptions);
 };
 
 function initializeRadar() {
+    // this function is invoke by 'public.js'
 
     let users = window.location.href.split('/#').pop();
     console.log("looking for publickeys in:", users);
@@ -47,11 +23,29 @@ function initializeRadar() {
         console.log("Production URL", url);
     }
 
+    
+    var margin = {top: 100, right: 100, bottom: 100, left: 100},
+        width = Math.min(700, window.innerWidth - 10) - margin.left - margin.right,
+        height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
+            
+    var color = d3.scaleOrdinal()
+        .range(["#ffffff","#f98e05","#00A0B0"]);
+        
+    var radarChartOptions = {
+      w: width,
+      h: height,
+      margin: margin,
+      maxValue: 0.5,
+      levels: 5,
+      roundStrokes: true,
+      color: color
+    };
+
     $.getJSON(url, function(data) {
     /* this is the format
          {axis:"Battery Life",value:0.22}, 
          {axis:"Brand",value:0.28},          */
         console.log("rendering radar graph with", data);
-        render(data.tops);
+        render(data.tops, radarChartOptions);
     });
 };
