090c100bb14dcb64ff356fbe91088a3c9c9ee91d
Claudio <claudio@tracking.exposed>
2019-12-15 02:47:03 +0100

    implemented a nice radar compare tool

--- content/compare.md
+++ content/compare.md
@@ -10,22 +10,26 @@ og_url: "https://pornhub.tracking.exposed/compare"
 og_description: "Analyze differences and correlations between categories and the personalized experiencesm, even if nobody uses or abuses PH anymore ;) Let's get some independent insights"
 ---
 
-
-<div class="container" >
-    <div class="radarChart" style="text-align: center;"></div>
-    <h4>
-        Radar graph: each radius represents one of the categories of the video watched ― This is still a demo: pick a random user below
-    </h4>
+<div class="container">
+    <span class="col-2" id="profiles" style="display:inline-block;vertical-align:top">
+        <smaller><i>Last active pseudonyms</i></smaller>
+        <button 
+            id="master"
+            type="button"
+            class="btn btn-secondary btn-lg btn-block cause--refresh"
+            hidden>
+            [placeholder]
+        </button>
+    </span>
+    <span
+        class="col-9 radarChart"
+        style="text-align: center;display:inline-block;">
+    </span> 
 </div>
 
-<div style="text-align:center; font-size:2em;">
-    <a href="/compare/#Hg5gp1kEVPMHEVvBkNrgbm7x6juj5LDc3rbosPMv1igA,CbM4wnicN9YJjAanTjHQzXJ7pJDdZnF3WQB5tmv4yFLY">1+2</a> ―
-    <a href="/compare/#Hg5gp1kEVPMHEVvBkNrgbm7x6juj5LDc3rbosPMv1igA,AaLK2xNWpr2QSr63snfdUS8kQQv2jCQXh6s51U6u3ffw">1+3</a> ―
-    <a href="/compare/#Hg5gp1kEVPMHEVvBkNrgbm7x6juj5LDc3rbosPMv1igA,4FNa2xBEzhnsyfR5HuvNvi89tms3AxAvdbbC9NtZn1Qw">1+4</a> ―
-    <a href="/compare/#CbM4wnicN9YJjAanTjHQzXJ7pJDdZnF3WQB5tmv4yFLY,AaLK2xNWpr2QSr63snfdUS8kQQv2jCQXh6s51U6u3ffw">2+3</a> ―
-    <a href="/compare/#CbM4wnicN9YJjAanTjHQzXJ7pJDdZnF3WQB5tmv4yFLY,4FNa2xBEzhnsyfR5HuvNvi89tms3AxAvdbbC9NtZn1Qw">2+4</a> ―
-    <a href="/compare/#AaLK2xNWpr2QSr63snfdUS8kQQv2jCQXh6s51U6u3ffw,4FNa2xBEzhnsyfR5HuvNvi89tms3AxAvdbbC9NtZn1Qw">3+4</a>
-</div>
+---
+
+## Radar graph: the radius represents one of the categories of the video watched
 
 The distance from the center of the point marked on the radius is given by the percentage of the times in which the categories appear. The points on the rays are joined with segments, so that the graph takes on a personal shape. This shape can tell us something about user profiling, radicalization and which are the categories most connected to each other.
 
@@ -36,6 +40,7 @@ The distance from the center of the point marked on the radius is given by the p
 
 <script>
 $(document).ready(function() {
+    generateButtons();
     initializeRadar();
 });
 </script>
--- static/js/radarLoader.js
+++ static/js/radarLoader.js
@@ -1,20 +1,85 @@
+function generateButtons() {
+
+    if (window.location.origin.match(/localhost/)) {
+        url='http://localhost:10000/api/v2/random';
+        console.log("Development URL", url);
+    } else {
+        url='/api/v2/random';
+        console.log("Production URL", url);
+    }
+
+    $.getJSON(url, function(data) {
+        _.each(data, addButton);
+        console.log("loaded", _.size(data), "users/buttons");
+        $(".cause--refresh").click(radarRefresh);
+        markDisabledButtons();
+    });
+};
+
+function addButton(userInfo) {
+    const entry = $("#master").clone();
+    const computedId = `u-${userInfo.publicKey}`;
+    entry.attr("id", computedId);
+    $("#profiles").append(entry);
+
+    const name = userInfo.p.replace(/-.*/, '');
+    $(entry).attr('trexlab', name);
+    entry.text(name);
+    entry.removeAttr('hidden');
+};
+
+function markDisabledButtons() {
+    $(".cause--refresh").removeAttr('disabled');
+    const users = _.compact(getUsers());
+    _.each(users, function(publicKey) {
+        $('#u-' + publicKey).attr('disabled', true);
+    });
+}
+
+function radarRefresh() {
+    const id = $(this).attr('id').substr(2);
+    const users = _.reverse(_.compact(getUsers()));
+    if(_.size(users) == 2)
+        users.pop();
+    users.push(id);
+
+    $('#u-' + id).text('Loading...');
+
+    window.history.pushState(null, "pornhub compare game", "/compare/#" + users.join(','));
+    console.log("updated href and marking the new publicKey as disabled buttons", name);
+    markDisabledButtons();
+    initializeRadar();
+}
+
+function getUsers() {
+    const option = window.location.href.split('/#').pop();
+    if(!option || _.size(option) < 30 ) {
+        console.log("failed first integrity check", option);
+        return [null, null];
+    }
+    const users = _.split(option, ',');
+    try {
+        if(_.size(users) != 2) {
+            console.log("failed second integrity check", users);
+            return [null, null];
+        }
+    } catch(e) {
+        console.log("handled error in getUsers: ", e);
+        return [ null, null ];
+    }
+    return users;
+}
 
 function initializeRadar() {
     // this function is invoke by 'public.js'
 
-    let users = window.location.href.split('/#').pop();
-    console.log("looking for publickeys in:", users);
-
-    if(_.size( users.split(',')) != 2) {
-        console.log("failed first integrity check", users);
-        return false;
-    }
+    const users = getUsers();
 
     if (window.location.origin.match(/localhost/)) {
-        url='http://localhost:10000/api/v1/radar/' + users;
+        url='http://localhost:10000/api/v1/radar/' + users.join(',');
         console.log("Development URL", url);
     } else {
-        url='/api/v1/radar/' + users;
+        url='/api/v1/radar/' + users.join(',');
         console.log("Production URL", url);
     }
 
@@ -39,7 +104,15 @@ function initializeRadar() {
     /* this is the format
          {axis:"Battery Life",value:0.22}, 
          {axis:"Brand",value:0.28},          */
-        console.log("rendering radar graph with", data);
-        render(data.tops, radarChartOptions);
+        console.log("rendering radar graph with", data, 'from', users);
+        RadarChart(".radarChart", data.tops, radarChartOptions);
+
+        const potentialLabel1 = $('#u-' + users[0]).attr('trexlab');
+        if(_.size(potentialLabel1))
+            $('#u-' + users[0]).text(potentialLabel1);
+
+        const potentialLabel2 = $('#u-' + users[1]).attr('trexlab');
+        if(_.size(potentialLabel2))
+            $('#u-' + users[1]).text(potentialLabel2);
     });
 };
